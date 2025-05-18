f4a171af72b9d899118376d13d166f071fdd03af
Elnur Shahbalayev <elnursahbalayev@gmail.com>
2025-05-18 20:09:35 +0400

    feat(tracker): implement DeepSORT tracking functionality
    - Add import statements for DeepSORT tracker and encoder
    - Create Tracker class encapsulating DeepSORT tracking functionality- Initialize DeepSORT tracker with specified maximum cosine distance and budget
    - Set up feature encoder using a pre-trained model

--- tracker.py
+++ tracker.py
@@ -1,1 +1,37 @@
-from deep
+from deep_sort.deep_sort.tracker import Tracker as DeepSortTracker
+from deep_sort.tools import generate_detections as gdet
+from deep_sort.deep_sort import nn_matching
+from deep_sort.deep_sort.detection import Detection
+import numpy as np
+
+class Tracker:
+    """
+    A tracking class that encapsulates object tracking functionality using DeepSORT.
+    It initializes the DeepSORT tracker and encoder with specific parameters.
+    """
+    tracker = None
+    encoder = None
+    tracks = None
+
+    def __init__(self):
+        """
+        Initializes the Tracker class.
+
+        It sets up the DeepSORT tracker with a specified maximum cosine distance and budget for the nearest neighbor metric.
+        Additionally, it initializes the feature encoder using a pre-trained model.
+        """
+        # Maximum cosine distance for track matching
+        max_cosine_distance = 0.4
+        # Budget for the nearest neighbor metric, None means unlimited
+        nn_budget = None
+
+        # File path for the feature encoder model
+        encoder_model_filename = 'model_data/mars-small128.pb'
+
+        # Initialize the nearest neighbor metric with the specified maximum cosine distance and budget
+        metric = nn_matching.NearestNeighborDistanceMetric('cosine', max_cosine_distance, nn_budget)
+        # Initialize the DeepSORT tracker with the above metric
+        self.tracker = DeepSortTracker(metric)
+        # Initialize the feature encoder with the specified model file and batch size
+        self.encoder = gdet.create_box_encoder(encoder_model_filename, batch_size=1)
+
