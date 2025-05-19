5481932f0650762bc7af21fe8881df0ab19935ba
Elnur Shahbalayev <elnursahbalayev@baykartech.org>
2025-05-19 11:50:11 +0400

    feat(tracking): add object tracking functionality to video processing- Implement object detection and tracking using YOLOv8 and Tracker- Add video processing pipeline to read, process, and write output video- Include object filtering based on confidence score- Visualize tracked objects with bounding boxes and unique colors- Update tracker.py with new tracking logic and Track class- Specify version numbers for dependencies in requirements files

--- tracker.py
+++ tracker.py
@@ -35,3 +35,46 @@ class Tracker:
         # Initialize the feature encoder with the specified model file and batch size
         self.encoder = gdet.create_box_encoder(encoder_model_filename, batch_size=1)
 
+
+    def update(self, frame, detections):
+
+        if len(detections) == 0:
+            self.tracker.predict()
+            self.tracker.update([])
+            self.update_tracks()
+            return
+
+        bboxes = np.asarray([d[:-1] for d in detections])
+        bboxes[:, 2:] = bboxes[:, 2:] - bboxes[:, 0:2]
+        scores = [d[-1] for d in detections]
+
+        features = self.encoder(frame, bboxes)
+
+        dets = []
+        for bbox_id, bbox in enumerate(bboxes):
+            dets.append(Detection(bbox, scores[bbox_id], features[bbox_id]))
+
+        self.tracker.predict()
+        self.tracker.update(dets)
+        self.update_tracks()
+
+    def update_tracks(self):
+        tracks = []
+        for track in self.tracker.tracks:
+            if not track.is_confirmed() or track.time_since_update > 1:
+                continue
+            bbox = track.to_tlbr()
+
+            id = track.track_id
+
+            track.append(Track(id, bbox))
+
+        self.tracks = tracks
+
+class Track:
+    track_id = None
+    bbox = None
+
+    def __init__(self, id, bbox):
+        self.track_id = id
+        self.bbox = bbox
